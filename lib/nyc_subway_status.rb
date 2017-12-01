0624b159edd6c83a6ea601b1f6e148e3b681da8c
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2017-11-30 22:01:55 -0500

    Try dummy hard-coded subway status, then hook up Nokogiri and eventually Watir

--- lib/nyc_subway_status.rb
+++ lib/nyc_subway_status.rb
@@ -1,5 +1,8 @@
 require "nyc_subway_status/version"
 require "nokogiri"
 require "open-uri"
+require "pry"
+require "watir"
+require "webdrivers"
 
 require_relative './nyc_subway_status/cli'
--- lib/nyc_subway_status/cli.rb
+++ lib/nyc_subway_status/cli.rb
@@ -1,7 +1,31 @@
 class NycSubwayStatus::CLI
 
 	def call
-		puts "Subway!"
+		 puts <<-eos
+Here's the current subway status:
+[1, 2, 3] 		Planned Work
+[4, 5, 6] 		Planned Work
+[7] 			Good Service
+[A, C, E] 		Planned Work
+[B, D, F, M] 		Planned Work
+[G]			Good Service
+[J, Z]			Good Service
+[L]		 	Planned Work
+[N, Q, R]		Planned Work
+[S]			Planned Work
+[Staten Island Railway]	Planned Work
+eos
 	end
 
+	def new_call(subway_url)
+		browser = Watir::Browser.new
+		browser.goto subway_url
+		doc = Nokogiri::HTML.parse(browser.html)
+		binding.pry
+		# puts doc.css("#subwayDiv img.subwayIcon_123").attribute("alt").text
+		# puts doc.css("div#123 a").text
+		#doc.css("#subwayDiv div div").children[0]
+		#doc.css("#subwayDiv div").children[3]
+		#doc.css(".subwayCategory")[1]
+	end
 end
