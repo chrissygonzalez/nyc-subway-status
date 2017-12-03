6c341e0048092fe4cd11c6dcd53c63a75f831a83
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2017-12-02 22:55:42 -0500

    Hard code links to name, status, and url (if url) for each train line

--- lib/nyc_subway_status/cli.rb
+++ lib/nyc_subway_status/cli.rb
@@ -20,20 +20,54 @@ eos
 	def new_call(subway_url)
 		browser = Watir::Browser.new
 		browser.goto subway_url
-		# trs = browser.trs
-		sleep(5)
+		sleep(3)
+
 		doc = Nokogiri::HTML.parse(browser.html)
+
+		train123name = doc.css("#subwayDiv div img")[0].attribute("alt").text
+		train123status = doc.css("#123").text
+		train123link = doc.css("#123 a").attribute("href").value if doc.css("#123 a").empty? != true
+
+		train456name = doc.css("#subwayDiv div img")[1].attribute("alt").text
+		train456status = doc.css("#456").text
+		train456link = doc.css("#456 a").attribute("href").value if doc.css("#456 a").empty? != true
+
+		train7name = doc.css("#subwayDiv div img")[2].attribute("alt").text
+		train7status = doc.css("#7").text
+		train7link = doc.css("#7 a").attribute("href").value if doc.css("#7 a").empty? != true
+
+		trainACEname = doc.css("#subwayDiv div img")[3].attribute("alt").text
+		trainACEstatus = doc.css("#ACE").text
+		trainACElink = doc.css("#ACE a").attribute("href").value if doc.css("#ACE a").empty? != true
+
+		trainBDFMname = doc.css("#subwayDiv div img")[4].attribute("alt").text
+		trainBDFMstatus = doc.css("#BDFM").text
+		trainBDFMlink = doc.css("#BDFM a").attribute("href").value if doc.css("#BDFM a").empty? != true
+
+		trainGname = doc.css("#subwayDiv div img")[5].attribute("alt").text
+		trainGstatus = doc.css("#G").text
+		trainGlink = doc.css("#G a").attribute("href").value if doc.css("#G a").empty? != true
+
+		trainJZname = doc.css("#subwayDiv div img")[6].attribute("alt").text
+		trainJZstatus = doc.css("#JZ").text
+		trainJZlink = doc.css("#JZ a").attribute("href").value if doc.css("#JZ a").empty? != true
+
+		trainLname = doc.css("#subwayDiv div img")[7].attribute("alt").text
+		trainLstatus = doc.css("#L").text
+		trainLlink = doc.css("#L a").attribute("href").value if doc.css("#L a").empty? != true
+
+		trainNQRWname = doc.css("#subwayDiv div img")[8].attribute("alt").text
+		trainNQRWstatus = doc.css("#NQRW").text
+		trainNQRWlink = doc.css("#NQRW a").attribute("href").value if doc.css("#NQRW a").empty? != true
+
+		trainSname = doc.css("#subwayDiv div img")[9].attribute("alt").text
+		trainSstatus = doc.css("#S").text
+		trainSlink = doc.css("#S a").attribute("href").value if doc.css("#S a").empty? != true
+
+		trainSIRname = doc.css("#subwayDiv div img")[10].attribute("alt").text
+		trainSIRstatus = doc.css("#SIR").text
+		trainSIRlink = doc.css("#SIR a").attribute("href").value if doc.css("#SIR a").empty? != true
+
 		binding.pry
-
-
-		train123name = doc.css("#subwayDiv td")[1].children[0].attribute("alt").value
-		train123status = doc.css("#subwayDiv td")[2].children.children.text
-
-		doc.css("#subwayDiv td").click
-		binding.pry
-		# doc.css("#subwayDiv td")[3].children[0].attribute("alt").value is 456 Subway
-		# 1 is 123, 3 is 456, 5 is 7...
-		# doc.css("#subwayDiv td")[6].children.children.text is Good Service for the 7
-		# if planned work, on new page, need content within <div id="status-contents">
 	end
 end
