581e22b63d001901a0841f0237bd483194d3c59c
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2017-12-03 21:41:00 -0500

    Remove fake new_call CLI method, replace with real Train scraping method

--- lib/nyc_subway_status/train.rb
+++ lib/nyc_subway_status/train.rb
@@ -1,7 +1,9 @@
 class NycSubwayStatus::Train
 	attr_accessor :name, :status, :url
 
-	TRAINS = ["123", "456", "7", "ACE", "BDFM", "G", "JZ", "L", "NQRW", "S", "SIR"]
+	TRAINS = ["123", "456", "7", "ACE", "BDFM", "G", "JZ", "L", "NQR", "S", "SIR"]
+
+	@@all = []
 
 	def self.scrape_trains
 		browser = Watir::Browser.new :chrome, headless: true
@@ -9,6 +11,18 @@ class NycSubwayStatus::Train
 		sleep(3)
 
 		doc = Nokogiri::HTML.parse(browser.html)
+
+		TRAINS.each_with_index { |line, index|
+			train = self.new
+			train.name = doc.css("#subwayDiv div img")[index].attribute("alt").text
+			train.status = doc.css("##{line}").text
+			train.url = doc.css("##{line} a").attribute("href").value if doc.css("##{line} a").empty? != true
+			@@all << train
+		}
 		binding.pry
+	end
+
+	def self.all
+		@@all
 	end
 end
